31364cac05abb293dd71804490b0d1ae4fee99d0
Shuvodip Das Himu <shuvodipdashimu@gmail.com>
2024-12-04 00:05:25 +0600

    learned

--- index.js
+++ index.js
@@ -1,37 +1,73 @@
-const minNum = 1;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+function add(x, y){
+  return x + y;
+}
 
-let attempts = 0;
-let guess;
-let running = true;
+function subtract(x, y){
+  return x - y;
+}
+function multiply(x, y){
+  return x * y;
+}
+function divide(x, y){
+  return x / y;
+}
 
-while(running){
 
-  guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
-  guess = Number(guess);
-  if(isNaN(guess)){
-    window.alert("Please enter a valid number");
-  }
-  else if(guess < minNum || guess > maxNum){
-    window.alert("Please enter a valid number");
-  }
-  else {
-    attempts++;
-    if(guess < answer){
-      window.alert("TOO LOW! TRY AGAIN!");
-    }
-    else if (guess > answer){
-      window.alert("TOO HIGH!");
-    }
-    else {
-      window.alert(`CONGRATULATIONS YOU GUESSED THE NUMBER IN ${attempts}  AND THE ANSWER WAS ${answer}`);
-      running = false;
-  }
-  }
+function isEven(number){
+  return number % 2 === 0 ? true : false;
+}
+function isValidEmail(email){
+  return email.includes("@") ? true : false;
+}
+
+console.log(isValidEmail("Shuvodipgmail.com"));
+
+
+// function happyBirthday(username, age){
+//   console.log("Happy Birthday to you");
+//   console.log("Happy Birthday to you");  
+//   console.log(`Happy Birthday dear ${username}!`);
+//   console.log(`You are ${age} years old!`);
+//   console.log("Happy Birthday to you");
+// }
+
+// happyBirthday("Shuvodip", 22);
+// happyBirthday("Goku", 68);
+// happyBirthday("Bulma", 77);
+// const minNum = 1;
+// const maxNum = 100;
+// const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+// let attempts = 0;
+// let guess;
+// let running = true;
+
+// while(running){
+
+//   guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+//   guess = Number(guess);
+//   if(isNaN(guess)){
+//     window.alert("Please enter a valid number");
+//   }
+//   else if(guess < minNum || guess > maxNum){
+//     window.alert("Please enter a valid number");
+//   }
+//   else {
+//     attempts++;
+//     if(guess < answer){
+//       window.alert("TOO LOW! TRY AGAIN!");
+//     }
+//     else if (guess > answer){
+//       window.alert("TOO HIGH!");
+//     }
+//     else {
+//       window.alert(`CONGRATULATIONS YOU GUESSED THE NUMBER IN ${attempts}  AND THE ANSWER WAS ${answer}`);
+//       running = false;
+//   }
+//   }
 
   
-}
+// }
 
 
 // for (let i = 1; i <= 20; i++){
